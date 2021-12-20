5100aea9e3838c85cf9bf58f4656155e3b12d328
Mohamed Noufal <getmohamednoufal@gmail.com>
2021-12-20 20:52:29 +0530

    changes made in public folder and manifest.json file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React from "react";
+import About from "./component/About/About";
+import Header from "./component/Header/Header";
 import TodoApp from "./component/TodoApp/TodoApp";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 export default function App() {
-    return <TodoApp />;
+    return (
+        <Router>
+            <Route path="/" component={Header} />
+            <Route path="/" exact component={TodoApp} />
+            <Route path="/about" component={About} />
+        </Router>
+    );
 }
